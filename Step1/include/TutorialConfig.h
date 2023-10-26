f18f1c2435bfd09de74fd284f9b46c9d980b7b00
Efim Shevrin <efim.shevrin@virtuozzo.com>
2023-10-27 00:20:09 +0800

    configure file

---
+++ Step1/include/TutorialConfig.h
@@ -0,0 +1,8 @@
+// the configured options and settings for Tutorial
+// TODO 10: Define Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR
+
+#include <string>
+
+const std::string tutorial_VERSION_MAJOR = 1;
+const std::string tutorial_VERSION_MINOR = 2;
+const std::string tutorial_VERSION_PATCH = 66;
